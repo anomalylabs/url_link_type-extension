37a8f2f2e744694084077f86848c92240b1b9920
Ryan Thompson <service@aiwebsystems.com>
2015-05-24 19:09:02 -0500

    Lang

--- resources/lang/en/field.php
+++ resources/lang/en/field.php
@@ -1,9 +1,19 @@
 <?php
 
 return [
-    'url' => [
+    'title'       => [
+        'name'         => 'Title',
+        'instructions' => 'Enter the link title.',
+        'placeholder'  => 'Company'
+    ],
+    'url'         => [
         'name'         => 'URL',
         'instructions' => 'Enter a URL. If you enter a URI, the site domain will be prepended automatically.',
-        'placeholder'  => 'about/company',
+        'placeholder'  => 'about/company'
     ],
+    'description' => [
+        'name'         => 'Description',
+        'instructions' => 'The optional description could be used for extra link text.',
+        'placeholder'  => 'About out company.'
+    ]
 ];
